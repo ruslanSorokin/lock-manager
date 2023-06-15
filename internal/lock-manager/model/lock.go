8b79995e1d12ac8aba35e6d835683a0deac739d3
Ruslan Sorokin <strawberryladder@gmail.com>
2023-06-15 12:20:56 +0300

    style: run go-fmt

--- internal/lock-manager/model/lock.go
+++ internal/lock-manager/model/lock.go
@@ -6,7 +6,7 @@ type Lock struct {
 	// ResourceID is an identefier of a locked resource.
 	ResourceID string
 	// SecretKey is a secret key you can use to unlock.
-	SecretKey  string
+	SecretKey string
 	// Lifetime is a duration during which this resource will be locked unless you unlock it first or renew it.
-	Lifetime   time.Duration
+	Lifetime time.Duration
 }
