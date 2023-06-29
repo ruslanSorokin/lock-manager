1ef1fd4078f7382cbf7428b1a346a22671f6b080
Ruslan Sorokin <strawberryladder@gmail.com>
2023-06-29 15:08:35 +0300

    refactor(Lock): remove ttl field

--- internal/lock-manager/model/lock.go
+++ internal/lock-manager/model/lock.go
@@ -1,12 +1,8 @@
 package model
 
-import "time"
-
 type Lock struct {
-	// ResourceID is an identefier of a locked resource.
+	// ResourceID is a unique identifier of a Lock.
 	ResourceID string
-	// SecretKey is a secret key you can use to unlock.
+	// SecretKey is a secret key you can use to unlock the resource.
 	SecretKey string
-	// Lifetime is a duration during which this resource will be locked unless you unlock it first or renew it.
-	Lifetime time.Duration
 }
