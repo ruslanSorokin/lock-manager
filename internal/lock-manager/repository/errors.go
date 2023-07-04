53f413846af93624d67795fec33a587dfea2b63d
Ruslan Sorokin <strawberryladder@gmail.com>
2023-07-04 10:30:04 +0300

    refactor(repository): make error messages lowercase

--- internal/lock-manager/repository/errors.go
+++ internal/lock-manager/repository/errors.go
@@ -3,7 +3,7 @@ package repository
 import "errors"
 
 var (
-	ErrLockAlreadyExists = errors.New("LockStorage: lock already exists")
-	ErrLockNotFound      = errors.New("LockStorage: lock is not found")
-	ErrInvalidToken      = errors.New("LockStorage: invalid token")
+	ErrLockAlreadyExists = errors.New("lock storage: lock already exists")
+	ErrLockNotFound      = errors.New("lock storage: lock is not found")
+	ErrInvalidToken      = errors.New("lock storage: invalid token")
 )
