8b6b81a9889d93a5311a3d981ae779e79b54da97
Ruslan Sorokin <strawberryladder@gmail.com>
2023-07-05 11:20:23 +0300

    dev: remove mockgen from Go tools

--- tools/tools.go
+++ tools/tools.go
@@ -1,10 +1,8 @@
 //go:build tools
 // +build tools
-
 package tools
 
 import (
-	_ "github.com/golang/mock/mockgen"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/incu6us/goimports-reviser/v3"
 	_ "golang.org/x/tools/cmd/goimports"
