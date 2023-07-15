4009d4d72214c1a9bddd0e48320b85ccad92624d
Ruslan Sorokin <strawberryladder@gmail.com>
2023-07-15 11:12:39 +0300

    docs(service): wrap variables in comments with "`"

--- internal/lock-manager/service/lock_service.go
+++ internal/lock-manager/service/lock_service.go
@@ -9,23 +9,25 @@ import (
 )
 
 type LockServiceI interface {
-	// Lock locks given resourceID and returns a token.
+	// Lock locks given `resourceID` and returns a token.
 	//
-	// If given resourceID is not valid, then service.ErrInvalidResourceID is returned.
-	//
-	// If given resourceID is already locked, then provider.ErrLockAlreadyExists is
+	// If given `resourceID` is not valid, then `service.ErrInvalidResourceID` is
 	// returned.
+	//
+	// If given `resourceID` is already locked, then
+	// `provider.ErrLockAlreadyExists` is returned.
 	Lock(ctx context.Context, resourceID string) (string, error)
 
-	// Unlock unlocks given resourceID only if token fits.
+	// Unlock unlocks given `resourceID` only if `token` fits.
 	//
-	// If given resourceID is not valid, then service.ErrInvalidResourceID is returned.
+	// If given `resourceID` is not valid, then `service.ErrInvalidResourceID` is
+	// returned.
 	//
-	// If given token is not valid, then service.ErrInvalidToken is returned.
+	// If given `token` is not valid, then `service.ErrInvalidToken` is returned.
 	//
-	// If there is no such lock, then provider.ErrLockNotFound is returned.
+	// If there is no such lock, then `provider.ErrLockNotFound` is returned.
 	//
-	// If given token doesn't fit, then provider.ErrWrongToken is returned.
+	// If given `token` doesn't fit, then `provider.ErrWrongToken` is returned.
 	Unlock(ctx context.Context, resourceID, token string) error
 }
 
