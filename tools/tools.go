e91a4c67e0251e44e4b7af11daab19bbc705391e
Ruslan Sorokin <strawberryladder@gmail.com>
2023-08-22 12:36:00 +0300

    style(tools): run gofumpt

--- tools/tools.go
+++ tools/tools.go
@@ -5,9 +5,9 @@ package tools
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/google/wire"
 	_ "github.com/incu6us/goimports-reviser/v3"
 	_ "github.com/vektra/mockery/v2"
-	_ "mvdan.cc/gofumpt"
 	_ "golang.org/x/tools/cmd/goimports"
-	_ "github.com/google/wire"
+	_ "mvdan.cc/gofumpt"
 )
